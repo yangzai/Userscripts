15769d596883a91560d504a87ae5923410bfeb50
Ang Hao Yang <yan9za1@gmail.com>
2014-09-24 01:42:31 +0800

    Added HideSignup for Hipvan.

---
+++ HideSignup.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name       HideSignup
+// @namespace  Hipvan
+// @version    0.1
+// @description  enter something useful
+// @match      http://*.hipvan.com/*
+// @copyright  2012+, You
+// ==/UserScript==
+
+
+$.colorbox.close();
+$('#colorbox').hide();
+$('#cboxOverlay').hide();
